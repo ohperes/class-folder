4eb4f249087404df5eb05ade38afaef36bba2f95
Vinicius Peres <vinicius.peres@ultralims.com.br>
2022-06-23 20:05:35 -0300

    Corrige responsividade do Login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -5,18 +5,20 @@ import styles from './Login.module.css';
 export function Login() {
 
   return (
-   <div className={styles.content}>
-        <img className={styles.loginLogo} src="../src/assets/amico.svg" alt="Logo de estudante" />
-        <h1 className={styles.title}>Class Folder</h1>
-        <p className={styles.subtitle}>Mantenha seus registros de aulas!</p>
-        
-        <div className={styles.form}>
-            <input type="text" placeholder='Usuário'/>
-            <input type="password" placeholder='Senha'/>
+    <div className={styles.container}>
+      <div className={styles.content}>
+            <img className={styles.loginLogo} src="../src/assets/amico.svg" alt="Logo de estudante" />
+            <h1 className={styles.title}>Class Folder</h1>
+            <p className={styles.subtitle}>Mantenha seus registros de aulas!</p>
+            
+            <div className={styles.form}>
+                <input type="text" placeholder='Usuário'/>
+                <input type="password" placeholder='Senha'/>
 
-            <button type="submit">Login</button>
-        </div>
-        <span>Novo por aqui?<Link to="/Registration">Cadastre-se!</Link></span>
+                <button type="submit">Login</button>
+            </div>
+            <span>Novo por aqui?<Link to="/Registration">Cadastre-se!</Link></span>
+      </div>
    </div>
   );
 }
--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -4,22 +4,24 @@ import styles from './Registration.module.css'
 export function Registration() {
 
   return (
-   <div className={styles.content}>
-        <img className={styles.loginLogo} src="../src/assets/amico.svg" alt="Logo de estudante" />
+    <div className={styles.container}>
+      <div className={styles.content}>
+            <img className={styles.loginLogo} src="../src/assets/amico.svg" alt="Logo de estudante" />
 
-        <h1 className={styles.title}>Class Folder</h1>
+            <h1 className={styles.title}>Class Folder</h1>
 
-        <p className={styles.subtitle}>Mantenha-se atualizado sobre os registros de aula!</p>
-        
-        <div className={styles.form}>
-            <input type="text" placeholder='Nome'/>
-            <input type="email" placeholder='Email'/>
-            <input type="numbe" placeholder='CPF'/>
-            <input type="password" placeholder='Senha'/>
+            <p className={styles.subtitle}>Mantenha-se atualizado sobre os registros de aula!</p>
+            
+            <div className={styles.form}>
+                <input type="text" placeholder='Nome'/>
+                <input type="email" placeholder='Email'/>
+                <input type="numbe" placeholder='CPF'/>
+                <input type="password" placeholder='Senha'/>
 
-            <button type="submit">Cadastrar</button>
-        </div>
-   </div>
+                <button type="submit">Cadastrar</button>
+            </div>
+      </div>
+    </div>
   );
 }
 
