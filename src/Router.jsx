0ca7aee6ac17a924ff2b3354ce6436c2d77ffe99
Julia Solano <juliaduartesolano@gmail.com>
2022-06-24 02:03:59 -0300

    first part of the profile

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,12 +4,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import { Login } from './pages/Login/index.jsx';
 import { Registration } from './pages/Registration/index.jsx';
+import Profile from './pages/UserProfile/index.jsx';
 
 export function Router() {
 	return (
 		<Routes>
 			<Route path="/" element={<Login />}/>
 			<Route path="/Registration" element={<Registration />}/>
+			<Route path="/Profile" element={<Profile />}/>
 		</Routes>
 	)
 }
---
+++ src/pages/UserProfile/index.jsx
@@ -0,0 +1,30 @@
+import styles from "./Profile.module.css";
+
+function Profile() {
+  return (
+    <div className={styles.container}>
+      <main>
+        <div className={styles.row}>
+          <div className={styles.left}>
+            <h4 className={styles.name}>Jane Doe</h4>
+            <p className={styles.info}>UI/UX Designer</p>
+            <p className={styles.info}>jane.doe@gmail.com</p>
+            <p className={styles.desc}>
+              Hi ! My name is Jane Doe. I'm a UI/UX Designer from Paris, in
+              France. I really enjoy photography and mountains.
+            </p>
+          </div>
+          <div className={styles.right}>
+            <ul className={styles.nav}>
+              <li>Perfil</li>
+              <li>Turmas</li>
+              <li>Aulas</li>
+            </ul>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Profile;
